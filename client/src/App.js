0d3d757ecd605844a11a4ae2c7e0a998c4f51045
shrushti2000 <shrushtipolekar@gmail.com>
2020-10-05 19:15:02 +0530

    added register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
+import Signup from './components/Signup/Signup.js';
 import Login from './components/Login/Login';
 
 function App() {
@@ -8,6 +8,7 @@ function App() {
     <div className='App'>
       <Router>
         <Switch>
+          <Route exact path='/' component={Signup} />
           <Route exact path='/login' component={Login} />
         </Switch>
       </Router>
