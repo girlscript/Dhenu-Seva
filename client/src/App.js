5265b690627dded62bbbd497c52e3227bd3e4c05
shrushti2000 <shrushtipolekar@gmail.com>
2020-10-08 11:19:29 +0530

    created homepage with post cards

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Signup from './components/Signup/Signup.js';
 import Login from './components/Login/Login';
+import HomePage from './components/HomePage/HomePage.js';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Switch>
           <Route exact path='/signup' component={Signup} />
           <Route exact path='/login' component={Login} />
+          <Route exact path='/' component={HomePage} />
         </Switch>
       </Router>
     </div>
