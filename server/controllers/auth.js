be9430e35cdee872e6c00ddd4fb463bd61583a8e
aashimgarg <18bcs001@smvdu.ac.in>
2020-10-12 16:29:08 +0530

    transformed then-catch to async-await for auth middleware

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,78 +3,64 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const configAuth = require('../config.json').auth;
 
-exports.signup = (req, res, next) => {
-  const email = req.body.email;
-  const name = req.body.name;
-  const password = req.body.password;
-  
-  User.findOne({ 
-    email: email
-   })
-  .then(savedUser => {
+exports.signup = async (req, res, next) => {
+  const {email,name,password} = req.body
+  try {
+    let savedUser = await User.findOne({ email })
     if (savedUser) {
       const error = new Error('User already exists. Please enter a new email');
       throw error;
     }
-    return  bcrypt.hash(password , configAuth.passwordEncryptionKeyLength)
-  })
-    .then(hashedPassword => {
-       const user = new User ({
-        email: email,
-        password: hashedPassword,
-        name: name,
-       });
-       return user.save();
-     })
-    .then(
-      result => {
-        res.status(201).json({ 
-          message: 'User created!',
-          userId: result._id
-        });
-      },
-    )
-    .catch((err) => {
-      console.log(err)
+    let hashedPassword = await bcrypt.hash(password, configAuth.passwordEncryptionKeyLength)
+    const user = new User({
+      email,
+      password: hashedPassword,
+      name,
     });
-};
+    let result = await user.save();
 
-exports.login = (req, res, next) => {
-  const email = req.body.email;
-  const password = req.body.password;
+    res.status(201).json({
+      message: 'User created!',
+      userId: result._id
+    })
+  }
+  catch (error) {
+    console.log(error)
+  }
+}
 
+exports.login = async (req, res, next) => {
+  const {email,password} = req.body
   let enteredUser;
+  try {
+    let user = await User.findOne({ email })
+    if (!user) {
+      const error = new Error('Enter valid credentials.');
+      throw error;
+    }
+    enteredUser = user;
 
-  User.findOne({ email: email })
-    .then(user => {
-      if (!user) {
-        const error = new Error('Enter valid credentials.');
-        throw error;
-      }
-      enteredUser = user;
-
-      return bcrypt.compare(password, user.password);
-    })
-    .then(isEqual => {
-      if (!isEqual) {
-        const error = new Error('Wrong password!');
-        throw error;
-      }
-      const token = jwt.sign(
-        {
-          email: enteredUser.email,
-          userId: enteredUser._id.toString()
-        },
-        // the secret key
-        configAuth.secretKey,
-        { expiresIn: '1h' }
-      );
-      res.status(200).json({ 
-        token: token, 
-        userId: enteredUser._id.toString() 
-      });
-    })
-    .catch(err => {
-      console.log(err)
+    let isEqual = await bcrypt.compare(password, user.password);
+    if (!isEqual) {
+      const error = new Error('Wrong password!');
+      throw error;
+    }
+    const token = jwt.sign(
+      {
+        email: enteredUser.email,
+        userId: enteredUser._id.toString()
+      },
+      // the secret key
+      configAuth.secretKey,
+      { expiresIn: '1h' }
+    );
+    res.status(200).json({
+      token: token,
+      userId: enteredUser._id.toString()
     });
-};
+  }
+
+  catch (error) {
+    console.log(error)
+  }
+}
