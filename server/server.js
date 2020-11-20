7ae4fa67e23bf431dfc07130abf62d859012d2d6
aashimgarg <18bcs001@smvdu.ac.in>
2020-11-20 21:45:01 +0530

    added cors package to avoid cors errors

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const header = require('./middlewares/header')
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 
 const config = require('./config.json')
