a1d33723617f59dfaa1e05a20adaa8634865a99e
aashimgarg <18bcs001@smvdu.ac.in>
2020-12-08 20:33:20 +0530

    added test case for authToken

--- server/test/middleware-auth.js
+++ server/test/middleware-auth.js
@@ -2,16 +2,33 @@ const expect = require('chai').expect;
 
 const authMiddleware = require('../middlewares/is-auth')
 
-it('should throw an error if no authorization header is present', function () {
-    const req = {
-        get : function(header) {
-           return null;
-        }
-    };
-    expect(authMiddleware.bind(
-        this,req,{},() => {}
-        )
-      ).to.throw(
-          'Not authenticated'
-        )
+describe('Auth Middleware' , function(){
+    it('should throw an error if no authorization header is present', function () {
+        const req = {
+            get : function(header) {
+               return null;
+            }
+        };
+        expect(authMiddleware.bind(
+            this,req,{},() => {}
+            )
+          ).to.throw(
+              'Not authenticated'
+            )
+    })
+
+    it('error if only one string is there' , function() {
+        const req = {
+            get : function(header) {
+                return 'abc';
+            }
+        };
+        expect(authMiddleware.bind(
+            this, req, {}, () => {}
+            )
+          ).to.throw();
+    
+    })
 })
+
+
